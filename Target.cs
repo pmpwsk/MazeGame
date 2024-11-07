d07685ecd36499860d6bf2d813670f44ba760c78
Flo <flo@uwap.org>
2024-11-07 16:12:00 +0100

    Added an animation when the game is done.

--- Target.cs
+++ Target.cs
@@ -8,9 +8,28 @@ public class Target : EnterEventThing
 
     public bool OnEnter(Thing sender)
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Done!");
+        FlashDone(
+        [
+            ConsoleColor.Green,
+            ConsoleColor.Yellow,
+            ConsoleColor.Green,
+            ConsoleColor.Yellow,
+            ConsoleColor.Green
+        ]);
         Console.ResetColor();
+        Console.WriteLine();
+
         return true;
+    }
+
+    private void FlashDone(ConsoleColor[] colors)
+    {
+        foreach (ConsoleColor color in colors)
+        {
+            Console.CursorLeft = 0;
+            Console.ForegroundColor = color;
+            Console.Write("Done!");
+            Thread.Sleep(500);
+        }
     }
 }
